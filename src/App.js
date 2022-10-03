65aeea38d499dc8c92198200932cfb696d62cd85
OnniRaisanen <t1raon00@students.oamk.fi>
2022-10-03 14:12:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [age, setAge] = useState(50)
+  const [upperrate, setUpperrate] = useState()
+  const [lowerrate, setLowerrate] = useState()
+  
+  function laske(e) {
+    e.preventDefault()
+    const tulos1 = (220 - age) *0.85
+    const tulos2 = (220 - age) *0.65
+    setUpperrate(tulos1)
+    setLowerrate(tulos2)
+  }
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='tausta'>
+      <h2>Heart rate limits calculator</h2>
+      <form onSubmit={laske}>
+        <div>
+          <label>Age</label>
+        </div>
+        <div>
+          <input type="number" value={age} onChange={e => setAge(e.target.value)}/>
+        </div>
+        <div>
+         <label>Heart rate limits</label>
+         </div>
+         <div id="text-box">
+         <output>{ lowerrate} - {upperrate} </output>
+        </div>
+        <button>Calculate</button>
+      </form>
+      
     </div>
   );
 }
